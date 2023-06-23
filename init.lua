727412da6d9ddadcba1046bbb5140f258a7a42fb
Nicolas Torneri <nicolas.torneri@gmail.com>
2023-06-23 20:56:26 +0200

    customize

--- init.lua
+++ init.lua
@@ -1,2 +1,7 @@
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>z", ":helpclose<cr>")
+vim.keymap.set("n", "<leader>f", ":Lexplore<cr>")
+vim.keymap.set("n", "<leader>h", ":nohlsearch<cr>")
