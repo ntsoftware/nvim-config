dbdf01c98790a1e1077191b25e4aa8db3219f1f0
Nicolas Torneri <nicolas.torneri@gmail.com>
2023-07-03 22:35:20 +0200

    customize

--- init.lua
+++ init.lua
@@ -2,6 +2,6 @@ vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.smartindent = true
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>z", ":helpclose<cr>")
-vim.keymap.set("n", "<leader>f", ":Lexplore<cr>")
+vim.keymap.set("n", "<leader>c", ":helpclose<cr>")
+vim.keymap.set("n", "<leader>e", ":Lexplore<cr>")
 vim.keymap.set("n", "<leader>h", ":nohlsearch<cr>")
